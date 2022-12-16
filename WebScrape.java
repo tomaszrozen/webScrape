e857b17a6817771a66a50605e86cf5a0b38626e1
Tomasz Rożeń <92816453+tomaszrozen@users.noreply.github.com>
2022-12-17 00:59:17 +0100

    Add files via upload

---
+++ WebScrape.java
@@ -0,0 +1,27 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class WebScrape {
+    public static void main(String[] args) {
+        final String url = "https://www.imdb.com/chart/top";
+        try {
+            Document document = Jsoup.connect(url).timeout(6000).get();
+            Elements element = document.select("tbody.lister-list");
+            for (Element e : element.select("tr")){
+                //String img = e.select("td.posterColumn img").attr("src");
+                String title = e.select("td.posterColumn img").attr("alt");
+                String year = e.select("td.titleColumn span.secondaryInfo").text().replaceAll("[^\\d]", "");
+                String rate = e.select("td.ratingColumn.imdbRating").text();
+                System.out.println(year);
+                System.out.println(rate);
+                System.out.println(title);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
